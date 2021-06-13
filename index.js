ac4de0b91b42250a7d0a2eaf4efe973ed8878b6a
redebron20 <redebron20@gmail.com>
2021-06-13 17:36:57 -0400

    commit tree ab5944b6483d07155bc2162c3e8f5be6fc1f0670

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter(driver => 
+       driver.toLowerCase() === name.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, name) {
+    return array.filter(driver => 
+        driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+     )
+}
+
+function matchName(array, string) {
+    return array.filter(driver => driver.name === string)
+}
